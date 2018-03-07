2870fe8d6a341ccb7945198e20902ac7d8a677e7
raissaesther <raissaesther@gmail.com>
2018-03-07 10:12:50 -0300

    mix

--- wp-content/themes/stockholm/js/custom_js.js
+++ wp-content/themes/stockholm/js/custom_js.js
@@ -4,10 +4,6 @@ var $j = jQuery.noConflict();
 $j(document).ready(function() {
 	"use strict";
 
-	// Instantiate MixItUp:
-	$j( '.filter a' ).click(function( event ) {
-	  event.preventDefault();
-	});
-	$j('#prod-container').mixItUp();
+	var mixer = mixitup('#prod-container');
 
 	});
