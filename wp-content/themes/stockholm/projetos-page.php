3cf5da9812f6f7b80cbcb53731a0ed511f47349f
raissaesther <raissaesther@gmail.com>
2018-03-08 11:47:39 -0300

    page

--- wp-content/themes/stockholm/projetos-page.php
+++ wp-content/themes/stockholm/projetos-page.php
@@ -3,16 +3,14 @@
 Template Name: Projetos e Ideias Page
 */
 ?>
-
-<?php get_header(); ?>
 <?php
 global $wp_query;
 $id = $wp_query->get_queried_object_id();
-if(get_post_meta($id, "qode_show-sidebar", true) == ''){
-		$sidebar = $qode_options['category_blog_sidebar'];
-}
-else{
-	$sidebar = get_post_meta($id, "qode_show-sidebar", true);
+$sidebar = get_post_meta($id, "qode_show-sidebar", true);
+
+$enable_page_comments = false;
+if(get_post_meta($id, "qode_enable-page-comments", true) == 'yes') {
+	$enable_page_comments = true;
 }
 
 if(get_post_meta($id, "qode_page_background_color", true) != ""){
@@ -21,31 +19,140 @@ if(get_post_meta($id, "qode_page_background_color", true) != ""){
 	$background_color = "";
 }
 
-?>
-
-	<?php if(get_post_meta($id, "qode_page_scroll_amount_for_sticky", true)) { ?>
-		<script>
-		var page_scroll_amount_for_sticky = <?php echo get_post_meta($id, "qode_page_scroll_amount_for_sticky", true); ?>;
-		</script>
-	<?php } ?>
-		<?php get_template_part( 'title' ); ?>
-	<?php
-	$revslider = get_post_meta($id, "qode_revolution-slider", true);
-	if (!empty($revslider)){ ?>
-		<div class="q_slider"><div class="q_slider_inner">
-		<?php echo do_shortcode($revslider); ?>
-		</div></div>
-	<?php
+$content_style = "";
+if(get_post_meta($id, "qode_content-top-padding", true) != ""){
+	if(get_post_meta($id, "qode_content-top-padding-mobile", true) == "yes"){
+		$content_style = "style='padding-top:".get_post_meta($id, "qode_content-top-padding", true)."px !important'";
+	}else{
+		$content_style = "style='padding-top:".get_post_meta($id, "qode_content-top-padding", true)."px'";
 	}
-	?>
+}
 
-	<div class="full_width">
-		<div class="full_width_inner">
+if ( get_query_var('paged') ) { $paged = get_query_var('paged'); }
+elseif ( get_query_var('page') ) { $paged = get_query_var('page'); }
+else { $paged = 1; }
 
-		<?php the_content(); ?>
+?>
+	<?php get_header(); ?>
+		<?php if(get_post_meta($id, "qode_page_scroll_amount_for_sticky", true)) { ?>
+			<script>
+			var page_scroll_amount_for_sticky = <?php echo get_post_meta($id, "qode_page_scroll_amount_for_sticky", true); ?>;
+			</script>
+		<?php } ?>
+			<?php get_template_part( 'title' ); ?>
+		<?php
+		$revslider = get_post_meta($id, "qode_revolution-slider", true);
+		if (!empty($revslider)){ ?>
+			<div class="q_slider"><div class="q_slider_inner">
+			<?php echo do_shortcode($revslider); ?>
+			</div></div>
+		<?php
+		}
+		?>
+	<div class="full_width"<?php if($background_color != "") { echo " style='background-color:". $background_color ."'";} ?>>
+	<div class="full_width_inner" <?php if($content_style != "") { echo wp_kses($content_style, array('style')); } ?>>
+		<?php if(($sidebar == "default")||($sidebar == "")) : ?>
+			<?php if (have_posts()) :
+					while (have_posts()) : the_post(); ?>
+					<?php the_content(); ?>
+					<?php
+ $args_pages = array(
+  'before'           => '<p class="single_links_pages">',
+  'after'            => '</p>',
+  'pagelink'         => '<span>%</span>'
+ );
 
-		</div>
+ wp_link_pages($args_pages); ?>
+
+					<?php endwhile; ?>
+				<?php endif; ?>
+		<?php elseif($sidebar == "1" || $sidebar == "2"): ?>
+
+			<?php if($sidebar == "1") : ?>
+				<div class="two_columns_66_33 clearfix grid2">
+					<div class="column1">
+			<?php elseif($sidebar == "2") : ?>
+				<div class="two_columns_75_25 clearfix grid2">
+					<div class="column1">
+			<?php endif; ?>
+					<?php if (have_posts()) :
+						while (have_posts()) : the_post(); ?>
+						<div class="column_inner">
+
+						<?php the_content(); ?>
+						<?php
+ $args_pages = array(
+  'before'           => '<p class="single_links_pages">',
+  'after'            => '</p>',
+  'pagelink'         => '<span>%</span>'
+ );
+
+ wp_link_pages($args_pages); ?>
+							<?php
+							if($enable_page_comments){
+							?>
+							<div class="container">
+								<div class="container_inner">
+							<?php
+								comments_template('', true);
+							?>
+								</div>
+							</div>
+							<?php
+							}
+							?>
+						</div>
+				<?php endwhile; ?>
+				<?php endif; ?>
+
+
+					</div>
+					<div class="column2"><?php get_sidebar();?></div>
+				</div>
+			<?php elseif($sidebar == "3" || $sidebar == "4"): ?>
+				<?php if($sidebar == "3") : ?>
+					<div class="two_columns_33_66 clearfix grid2">
+						<div class="column1"><?php get_sidebar();?></div>
+						<div class="column2">
+				<?php elseif($sidebar == "4") : ?>
+					<div class="two_columns_25_75 clearfix grid2">
+						<div class="column1"><?php get_sidebar();?></div>
+						<div class="column2">
+				<?php endif; ?>
+						<?php if (have_posts()) :
+							while (have_posts()) : the_post(); ?>
+							<div class="column_inner">
+							<?php the_content(); ?>
+							<?php
+ $args_pages = array(
+  'before'           => '<p class="single_links_pages">',
+  'after'            => '</p>',
+  'pagelink'         => '<span>%</span>'
+ );
+
+ wp_link_pages($args_pages); ?>
+							<?php
+							if($enable_page_comments){
+							?>
+							<div class="container">
+								<div class="container_inner">
+							<?php
+								comments_template('', true);
+							?>
+								</div>
+							</div>
+							<?php
+							}
+							?>
+							</div>
+					<?php endwhile; ?>
+					<?php endif; ?>
+
+
+						</div>
+
+					</div>
+			<?php endif; ?>
 	</div>
-
-
-<?php get_footer(); ?>
+	</div>
+	<?php get_footer(); ?>
