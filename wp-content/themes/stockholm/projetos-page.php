1acd167667ba743990c5adc0bb76a7f9d661a523
raissaesther <raissaesther@gmail.com>
2018-03-08 11:27:50 -0300

    page

--- wp-content/themes/stockholm/projetos-page.php
+++ wp-content/themes/stockholm/projetos-page.php
@@ -38,7 +38,6 @@ if(get_post_meta($id, "qode_page_background_color", true) != ""){
 	<?php
 	}
 	?>
-<?php if($qode_options['blog_style'] == 5){ ?>
 
 	<div class="full_width">
 		<div class="full_width_inner">
@@ -50,43 +49,5 @@ if(get_post_meta($id, "qode_page_background_color", true) != ""){
 		</div>
 	</div>
 
-	<?php } else { ?>
 
-	<div class="container"<?php if($background_color != "") { echo " style='background-color:". $background_color ."'";} ?>>
-		<div class="container_inner default_template_holder clearfix">
-			<?php if(($sidebar == "default")||($sidebar == "")) : ?>
-				<?php
-					get_template_part('templates/blog/blog', 'structure');
-				?>
-			<?php elseif($sidebar == "1" || $sidebar == "2"): ?>
-				<div class="<?php if($sidebar == "1"):?>two_columns_66_33<?php elseif($sidebar == "2") : ?>two_columns_75_25<?php endif; ?> background_color_sidebar grid2 clearfix">
-					<div class="column1">
-						<div class="column_inner">
-							<?php
-								get_template_part('templates/blog/blog', 'structure');
-							?>
-						</div>
-					</div>
-					<div class="column2">
-						<?php get_sidebar(); ?>
-					</div>
-				</div>
-		<?php elseif($sidebar == "3" || $sidebar == "4"): ?>
-				<div class="<?php if($sidebar == "3"):?>two_columns_33_66<?php elseif($sidebar == "4") : ?>two_columns_25_75<?php endif; ?> background_color_sidebar grid2 clearfix">
-					<div class="column1">
-					<?php get_sidebar(); ?>
-					</div>
-					<div class="column2">
-						<div class="column_inner">
-							<?php
-								get_template_part('templates/blog/blog', 'structure');
-							?>
-						</div>
-					</div>
-				</div>
-			<?php endif; ?>
-		</div>
-	</div>
-
-	<?php } ?>
 <?php get_footer(); ?>
