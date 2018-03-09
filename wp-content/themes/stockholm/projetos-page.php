a96283795919f3f2a908eb3a1941cf85873b82f1
raissaesther <raissaesther@gmail.com>
2018-03-09 18:39:14 -0300

    dstk

--- wp-content/themes/stockholm/projetos-page.php
+++ wp-content/themes/stockholm/projetos-page.php
@@ -92,7 +92,7 @@ if (!empty($revslider)){ ?>
 									</header>
 									<section style="overflow: hidden;" class="wpb_column vc_column_container">
 										<div class="vc_col-sm-12" style="overflow:hidden">
-											<img style="max-width: 100%;float: left;margin: 0 20px 0 0;" src="<?php echo $image[0]; ?>">
+											<img style="max-width: 50%;float: left;margin: 0 20px 0 0;" src="<?php echo $image[0]; ?>">
 
 											<p><?php the_content(); ?></p>
 										</div>
