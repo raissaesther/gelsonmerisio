ece16d7a20aa3d775f5e82adcf2b290a242866a5
raissaesther <raissaesther@gmail.com>
2018-03-12 12:14:32 -0300

    git

--- wp-content/themes/stockholm/js/custom_js.js
+++ wp-content/themes/stockholm/js/custom_js.js
@@ -2,31 +2,19 @@
 var $j = jQuery.noConflict();
 
 $j(document).ready(function() {
-"use strict";
+	"use strict";
 
-// Instantiate MixItUp:
-$j( '.filter a' ).click(function( event ) {
-  event.preventDefault();
-});
+	$j( '.filter a' ).click(function( event ) {
+		event.preventDefault();
+	});
 
-
-
-var containerEl = document.querySelector('#prod-container');
- var mixer;
-
- if (containerEl) {
-		 mixer = mixitup(containerEl, {
-
-
-		 });
- }
 
 	$j('.open-popup-link').magnificPopup({
 		type: 'inline',
 		gallery: {
-		    // options for gallery
-		    enabled: true
-		  }
+			// options for gallery
+			enabled: true
+		}
 	});
 
 	$j(document).on('click', '.popup-modal-dismiss', function (e) {
@@ -37,3 +25,14 @@ var containerEl = document.querySelector('#prod-container');
 
 
 });
+// Instantiate MixItUp:
+
+var containerEl = document.querySelector('#prod-container');
+var mixer;
+
+if (containerEl) {
+	mixer = mixitup(containerEl, {
+
+
+	});
+}
