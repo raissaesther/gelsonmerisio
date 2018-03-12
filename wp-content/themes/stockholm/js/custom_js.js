1dbf28388c2bfa39f798e63244f2de8e73580be4
raissaesther <raissaesther@gmail.com>
2018-03-12 10:16:07 -0300

    error js

--- wp-content/themes/stockholm/js/custom_js.js
+++ wp-content/themes/stockholm/js/custom_js.js
@@ -2,36 +2,39 @@
 var $j = jQuery.noConflict();
 
 $j(document).ready(function() {
-	"use strict";
+"use strict";
 
-	// Instantiate MixItUp:
-	$j( '.filter a' ).click(function( event ) {
-	  event.preventDefault();
+// Instantiate MixItUp:
+$j( '.filter a' ).click(function( event ) {
+  event.preventDefault();
+});
+
+
+
+var containerEl = document.querySelector('#prod-container');
+ var mixer;
+
+ if (containerEl) {
+		 mixer = mixitup(containerEl, {
+					selectors: {
+							control: '[data-mixitup-control]'
+					}
+		 });
+ }
+
+	$j('.open-popup-link').magnificPopup({
+		type: 'inline',
+		gallery: {
+		    // options for gallery
+		    enabled: true
+		  }
 	});
 
-	var containerEl = document.querySelector('#prod-container');
-
-	var mixer = mixitup(containerEl, {
-	    animation: {
-	        effects: 'fade scale(0.5)'
-	    }
+	$j(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$j.magnificPopup.close();
 	});
 
 
 
-		$j('.open-popup-link').magnificPopup({
-			type: 'inline',
-			gallery: {
-			    // options for gallery
-			    enabled: true
-			  }
-		});
-
-		$j(document).on('click', '.popup-modal-dismiss', function (e) {
-			e.preventDefault();
-			$j.magnificPopup.close();
-		});
-
-
-
-	});
+});
